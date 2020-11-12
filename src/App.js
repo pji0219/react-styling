1d644ebc2a39cc6cdea0cd981cf7c8898d330f85
park jong ik <pji219@naver.com>
2020-11-12 23:51:31 +0900

    fullWidth

--- src/App.js
+++ src/App.js
@@ -37,6 +37,11 @@ function App() {
           <Button2 color="pink" size="medium" outline>BUTTON</Button2>
           <Button2 color="gray" size="large" outline>BUTTON</Button2> {/* outline = outline={true} */}
         </ButtonGroup>
+        <ButtonGroup>
+          <Button2 color="blue" size="large" fullWidth>BUTTON</Button2>
+          <Button2 color="pink" size="large" fullWidth>BUTTON</Button2>
+          <Button2 color="gray" size="large" fullWidth>BUTTON</Button2>
+        </ButtonGroup>
       </AppBlock>
    </ThemeProvider>
   );
